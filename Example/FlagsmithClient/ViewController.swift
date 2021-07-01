1acd7004994d325baddb6a2262059f0ae88f2672
Daniel Wichett <wicheda@gmail.com>
2021-07-01 11:29:36 +0100

    Renaming to Flagsmith

---
+++ Example/FlagsmithClient/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FlagsmithClient
+//
+//  Created by Tomash Tsiupiak on 06/20/2019.
+//  Copyright (c) 2019 Tomash Tsiupiak. All rights reserved.
+//
+
+import UIKit
+import FlagsmithClient
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+}
