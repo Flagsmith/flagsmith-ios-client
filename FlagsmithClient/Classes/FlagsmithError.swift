3e966ee745c9eea3e575ee44699e1e20760c4ce8
Richard Piazza <richard@richardpiazza.com>
2022-03-21 11:45:11 -0500

    Consolidated Error Handling (#20)

---
+++ FlagsmithClient/Classes/FlagsmithError.swift
@@ -0,0 +1,61 @@
+//
+//  FlagsmithError.swift
+//  FlagsmithClient
+//
+//  Created by Richard Piazza on 3/18/22.
+//
+
+import Foundation
+
+/// All errors that can be encountered while using the **FlagsmithClient**
+public enum FlagsmithError: LocalizedError {
+    /// API Key was not provided or invalid.
+    case apiKey
+    /// API URL was invalid.
+    case apiURL(String)
+    /// API request could not be encoded.
+    case encoding(EncodingError)
+    /// API Response status code was not expected.
+    case statusCode(Int)
+    /// API Response could not be decoded.
+    case decoding(DecodingError)
+    /// Unknown or unhandled error was encountered.
+    case unhandled(Error)
+    
+    public var errorDescription: String? {
+        switch self {
+        case .apiKey:
+            return "API Key was not provided or invalid"
+        case .apiURL(let path):
+            return "API URL '\(path)' was invalid"
+        case .encoding(let error):
+            return "API Request could not be encoded: \(error.localizedDescription)"
+        case .statusCode(let code):
+            return "API Status Code '\(code)' was not expected."
+        case .decoding(let error):
+            return "API Response could not be decoded: \(error.localizedDescription)"
+        case .unhandled(let error):
+            return "An unknown or unhandled error was encountered: \(error.localizedDescription)"
+        }
+    }
+    
+    /// Initialize a `FlagsmithError` using an existing `Swift.Error`.
+    ///
+    /// The error provided will be processed in several ways:
+    /// * as `FlagsmithError`: The instance will be directly assigned.
+    /// * as `EncodingError`: `.encoding()` error will be created.
+    /// * as `DecodingError`: `.decoding()` error will be created.
+    /// * default: `.unhandled()` error will be created.
+    internal init(_ error: Error) {
+        switch error {
+        case let flagsmithError as FlagsmithError:
+            self = flagsmithError
+        case let encodingError as EncodingError:
+            self = .encoding(encodingError)
+        case let decodingError as DecodingError:
+            self = .decoding(decodingError)
+        default:
+            self = .unhandled(error)
+        }
+    }
+}
---
+++ FlagsmithClient/Classes/Internal/Router.swift
@@ -0,0 +1,97 @@
+//
+//  Router.swift
+//  FlagsmithClient
+//
+//  Created by Richard Piazza on 3/18/22.
+//
+
+import Foundation
+
+enum Router {
+  private enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+  }
+  
+  case getFlags
+  case getIdentity(identity: String)
+  case postTrait(trait: Trait, identity: String)
+  case postAnalytics(events: [String:Int])
+  
+  private var method: HTTPMethod {
+    switch self {
+    case .getFlags, .getIdentity:
+      return .get
+    case .postTrait, .postAnalytics:
+      return .post
+    }
+  }
+  
+  private var path: String {
+    switch self {
+    case .getFlags:
+      return "flags/"
+    case .getIdentity:
+      return "identities/"
+    case .postTrait:
+      return "traits/"
+    case .postAnalytics:
+      return "analytics/flags/"
+    }
+  }
+
+  private var parameters: [URLQueryItem]? {
+    switch self {
+    case .getIdentity(let identity):
+      return [URLQueryItem(name: "identifier", value: identity)]
+    default:
+      return nil
+    }
+  }
+
+  private func body(using encoder: JSONEncoder) throws -> Data? {
+    switch self {
+    case .getFlags, .getIdentity:
+      return nil
+    case .postTrait(let trait, let identifier):
+      let traitWithIdentity = Trait(trait: trait, identifier: identifier)
+      return try encoder.encode(traitWithIdentity)
+    case .postAnalytics(let events):
+      return try encoder.encode(events)
+    }
+  }
+  
+  /// Generate a `URLRequest` with headers and encoded body.
+  ///
+  /// - parameters:
+  ///  - baseUrl: The base URL of the api on which to base the request.
+  ///  - apiKey: The organization key to provide in the request headers.
+  ///  - encoder: `JSONEncoder` used to encode the request body.
+  func request(baseUrl: URL,
+               apiKey: String,
+               using encoder: JSONEncoder = JSONEncoder()
+  ) throws -> URLRequest {
+    let urlString = baseUrl.appendingPathComponent(path).absoluteString
+    var urlComponents = URLComponents(string: urlString)
+    urlComponents?.queryItems = parameters
+    guard let url = urlComponents?.url else {
+      // This is unlikely to ever be hit, but it is safer than
+      // relying on the forcefully-unwrapped optional.
+      throw FlagsmithError.apiURL(urlString)
+    }
+    
+    guard !url.isFileURL else {
+      throw FlagsmithError.apiURL(urlString)
+    }
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = method.rawValue
+    if let body = try self.body(using: encoder) {
+      request.httpBody = body
+    }
+    request.addValue(apiKey, forHTTPHeaderField: "X-Environment-Key")
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    return request
+  }
+}
