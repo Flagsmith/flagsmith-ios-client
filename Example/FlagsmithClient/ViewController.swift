7fe38ca1213a602622461376ff000d0da1e53a70
Gareth Reese <gareth@foresightmobile.com>
2024-05-15 11:25:45 +0100

    Update example app to support ATT and privacy status to try to reproduce the customer's issue

--- Example/FlagsmithClient/ViewController.swift
+++ Example/FlagsmithClient/ViewController.swift
@@ -8,7 +8,33 @@
 
 import UIKit
 import FlagsmithClient
+import AppTrackingTransparency
+import AdSupport
 
 class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      // Request user authorization for tracking
+      ATTrackingManager.requestTrackingAuthorization { status in
+          switch status {
+          case .authorized:
+              // Tracking authorization granted
+              print("Tracking authorization granted")
+          case .denied:
+              // Tracking authorization denied
+              print("Tracking authorization denied")
+          case .restricted:
+              // Tracking restricted
+              print("Tracking restricted")
+          case .notDetermined:
+              // Tracking authorization not determined
+              print("Tracking authorization not determined")
+          @unknown default:
+              // Handle other cases
+              print("Unknown tracking authorization status")
+          }
+      }
+  }
 
 }
