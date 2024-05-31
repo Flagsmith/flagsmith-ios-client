af668ac24712c2171e0f4928700178251699dcee
Jack Walker <jack@foresightmobile.com>
2024-05-31 10:29:34 +0100

    Fix formatting

--- FlagsmithClient/Classes/Flagsmith.swift
+++ FlagsmithClient/Classes/Flagsmith.swift
@@ -276,8 +276,8 @@ public final class Flagsmith: @unchecked Sendable {
     ///   - identity: ID of the user
     ///   - completion: Closure with Result which contains a list of Flags in case of success or Error in case of failure
     public func getIdentityFlags(_ traits: [Trait],
-                          forIdentity identity: String,
-                          completion: @Sendable @escaping (Result<[Flag], any Error>) -> Void)
+                                 forIdentity identity: String,
+                                 completion: @Sendable @escaping (Result<[Flag], any Error>) -> Void)
     {
         apiManager.request(.postTraits(identity: identity, traits: traits)) { (result: Result<Traits, Error>) in
             completion(result.map(\.flags))
